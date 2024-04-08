fbf9bf789f1315b41792aeaf074c2dace2a351a1
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-04-08 11:50:26 +0200

    creating first microservice

---
+++ customer/src/main/java/com/marlon/customer/CustomerApplication.java
@@ -0,0 +1,14 @@
+package com.marlon.customer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
