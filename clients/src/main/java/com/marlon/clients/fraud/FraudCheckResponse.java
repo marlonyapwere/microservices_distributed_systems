36bcfa376970738e7811116b5ce9b2b90ed27a9e
marlone-nyapwere <marloneenyapwere@gmail.com>
2024-04-09 18:37:37 +0200

    feign client

---
+++ clients/src/main/java/com/marlon/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,5 @@
+package com.marlon.clients.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+
+}
---
+++ clients/src/main/java/com/marlon/clients/fraud/Main.java
@@ -0,0 +1,7 @@
+package com.marlon.clients.fraud;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
